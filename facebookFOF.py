974511506dd13ba49ba18c295fbadf4b8761c10a
EliotAndres <eliot.andres@eleves.enpc.fr>
2018-04-30 16:18:05 +0100

    first commit

---
+++ facebookFOF.py
@@ -0,0 +1,125 @@
+from html.parser import HTMLParser
+import re
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import os
+from tqdm import tqdm
+import pickle
+import getpass
+
+username = input("Facebook username:")
+password = getpass.getpass('Password:')
+
+chrome_options = webdriver.ChromeOptions()
+prefs = {"profile.default_content_setting_values.notifications" : 2}
+chrome_options.add_experimental_option("prefs",prefs)
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+driver.get('http://www.facebook.com/')
+
+# authenticate to facebook account
+elem = driver.find_element_by_id("email")
+elem.send_keys(username)
+elem = driver.find_element_by_id("pass")
+elem.send_keys(password)
+elem.send_keys(Keys.RETURN)
+time.sleep(5)
+
+
+def get_fb_page(url):
+    time.sleep(2)
+    driver.get(url)
+    pos = -1
+    pos_prev = -1
+    scroll_height = 0
+    while pos != scroll_height:
+        driver.execute_script("window.scrollBy(0,5000)");
+        scroll_height = driver.execute_script("return document.body.scroll_height")
+        pos = pos_prev
+        pos_prev = scroll_height
+        time.sleep(2)
+
+    html_source = driver.page_source
+    return html_source
+
+
+def find_friend_from_url(url):
+    if re.search('com\/profile.php\?id=\d+\&', url) is not None:
+        m = re.search('com\/profile.php\?id=(\d+)\&', url)
+        friend = m.group(1)
+    else:
+        m = re.search('com\/(.*)\?', url)
+        friend = m.group(1)
+    return friend
+
+
+class MyHTMLParser(HTMLParser):
+    urls = []
+
+    def error(self, message):
+        pass
+
+    def handle_starttag(self, tag, attrs):
+        # Only parse the 'anchor' tag.
+        if tag == "a":
+            # Check the list of defined attributes.
+            for name, value in attrs:
+                # If href is defined, print it.
+                if name == "href":
+                    if re.search('\?href|&href|hc_loca|\?fref', value) is not None:
+                        if re.search('.com/pages', value) is None:
+                            self.urls.append(value)
+
+
+my_url = 'http://www.facebook.com/' + username + '/friends'
+
+UNIQ_FILENAME = 'uniq_urls.pickle'
+if os.path.isfile(UNIQ_FILENAME):
+    with open(UNIQ_FILENAME, 'rb') as f:
+        uniq_urls = pickle.load(f)
+    print('We loaded {} uniq friends'.format(len(uniq_urls)))
+else:
+    friends_page = get_fb_page(my_url)
+    parser = MyHTMLParser()
+    parser.feed(friends_page)
+    uniq_urls = set(parser.urls)
+
+    print('We found {} friends, saving it'.format(len(uniq_urls)))
+
+    with open(UNIQ_FILENAME, 'wb') as f:
+        pickle.dump(uniq_urls, f)
+
+friend_graph = {}
+GRAPH_FILENAME = 'friend_graph.pickle'
+
+if os.path.isfile(GRAPH_FILENAME):
+    with open(GRAPH_FILENAME, 'rb') as f:
+        friend_graph = pickle.load(f)
+    print('Loaded existing graph, found {} keys'.format(len(friend_graph.keys())))
+
+
+
+for url in tqdm(uniq_urls):
+    friend_username = find_friend_from_url(url)
+    if friend_username in friend_graph.keys():
+        continue
+
+    friend_graph[friend_username] = [username]
+    mutual_url = 'https://www.facebook.com/{}/friends_mutual'.format(friend_username)
+    mutual_page = get_fb_page(mutual_url)
+
+    parser = MyHTMLParser()
+    parser.urls = []
+    parser.feed(mutual_page)
+    mutual_friends_urls = set(parser.urls)
+    print('Found {} urls'.format(len(mutual_friends_urls)))
+
+    for mutual_url in mutual_friends_urls:
+        mutual_friend = find_friend_from_url(mutual_url)
+        friend_graph[friend_username].append(mutual_friend)
+
+    with open(GRAPH_FILENAME, 'wb') as f:
+        pickle.dump(friend_graph, f)
+
+driver.quit()
